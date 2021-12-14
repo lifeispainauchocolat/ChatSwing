2c7594facdb1de28ab54575ad1d3b94f16fadfd1
maxmk <max.mathiszik@gmail.com>
2021-12-14 11:42:15 +0100

    Initial commit

---
+++ src/Chat.java
@@ -0,0 +1,105 @@
+import socketio.Socket;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class Chat {
+    private JPanel panel1;
+    private JTextField chatInput;
+    private JButton sendButton;
+    private JTextField inputIP;
+    private JButton startButton;
+    private JScrollPane chatWindow1;
+    private JTextPane chatWindow;
+    private String ipAddress;
+
+    private Socket client;
+    private Thread thread;
+    public JTextPane getChatWindow() {
+        return chatWindow;
+    }
+
+    public void setChatWindow(JTextPane chatWindow) {
+        this.chatWindow = chatWindow;
+    }
+
+    public Chat() {
+        //thread = new Thread(new Aktualisieren(this,client));
+        //thread.start();
+        //if (thread.isAlive()){
+        //   System.out.println("Thread started!");
+        //}
+        Chat tempChat = this;
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ipAddress=inputIP.getText();
+                String[] tempAr = ipAddress.split(":");
+                inputIP.setVisible(false);
+                startButton.setVisible(false);
+                try {client = new Socket(tempAr[0], Integer.parseInt(tempAr[1]) );} catch (IOException ex) {}
+                if(client.connect()){
+                    chatWindow.setText("connceted to "+ipAddress);
+                }
+                thread = new Thread(new Aktualisieren(client,tempChat));
+                thread.start();
+
+            }
+        });
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            String temp = chatInput.getText();
+            chatInput.setText("");
+                //System.out.println(temp);
+            try {
+                client.write(temp+"\n");
+            }catch (Exception e1){}
+            }
+        });
+
+    }
+
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Chat");
+        Chat chat = new Chat();
+        frame.setContentPane(chat.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+}
+
+class Aktualisieren implements Runnable{
+
+    private Socket client;
+    private String temp1;
+    private Chat chat;
+    //private JTextPane chatWindow;
+    public Aktualisieren(Socket client, Chat chat){
+        this.client=client;
+        //this.chatWindow = jTextPane;
+        //this.chatWindow=chatWindow;
+        this.chat=chat;
+    }
+    @Override
+    public void run() {
+
+            try{
+
+                while(!(temp1 = client.readLine()).contains("exit")){
+                    JTextPane tempPane = new JTextPane();
+                    tempPane.setText(chat.getChatWindow().getText()+temp1);
+                    chat.setChatWindow(tempPane);
+                    //Chat.chatWindow.setText(Chat.chatWindow.getText()+temp1);
+                    //chatWindow.updateUI();
+                }
+            }catch (Exception e){}
+
+    }
+}
---
+++ src/MultiEchoServer.java
@@ -0,0 +1,93 @@
+import socketio.ServerSocket;
+import socketio.Socket;
+
+import java.util.ArrayList;
+
+public class MultiEchoServer{
+   private int port = 1234;
+
+   private ServerSocket server;
+   private ArrayList<Socket> myClients;
+   private ArrayList<Thread> myThreads;
+   private String tempChat;
+
+   public MultiEchoServer()throws Exception{
+       server = new ServerSocket(port);
+       myThreads = new ArrayList<>();
+       myClients = new ArrayList<>();
+
+       while(true){
+           Socket client = server.accept();
+           myClients.add(client);
+           System.out.println("Client "+myClients.size()+" accepted!");
+           Thread t = new Thread(new Kommunikation(client,myClients.size(),myClients));
+           myThreads.add(t);
+           t.start();
+           System.out.println("Thread "+myThreads.size()+" started!");
+
+       }
+
+       /*System.out.println("waiting for client...");
+       client = server.accept();
+       System.out.println("client accepted!");*/
+
+   }
+
+   /*public void kommunizieren() throws Exception{
+       String temp;
+       while(!(temp = client.readLine()).contains("exit")){
+            client.write("echo: "+temp+"\n");
+
+        }
+       System.out.println("closing connection...");
+        beenden();
+
+   }*/
+
+
+
+   public static void main(String[] args)throws Exception{
+       new MultiEchoServer();
+
+   }
+
+
+
+
+
+}
+
+class Kommunikation implements Runnable{
+
+    private Socket client;
+    private int clientNR;
+    private ArrayList<Socket> clientList;
+
+    public Kommunikation(Socket client, int clientNR,ArrayList<Socket> clientList){
+       this.client=client;
+       this.clientNR=clientNR;
+       this.clientList=clientList;
+    }
+
+    @Override
+    public void run() {
+        try{kommunizieren();}catch(Exception e){}
+    }
+
+    public void kommunizieren()throws Exception{
+        String temp;
+        while(!(temp = client.readLine()).contains("exit")){
+            System.out.println(temp);
+            for (Socket tempClient:clientList) {
+                tempClient.write("user "+clientNR+": "+temp+"\n");
+            }
+
+        }
+        System.out.println("closing connection...");
+        beenden();
+    }
+
+    public void beenden()throws Exception{
+        client.close();
+    }
+}
